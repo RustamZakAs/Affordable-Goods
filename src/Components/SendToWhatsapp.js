177897a10cf7a4dd77d92a2c401bcddb9d17ef48
RustamZakAs <rostik055@gmail.com>
2023-01-18 18:13:03 +0400

    changes and build

--- src/Components/SendToWhatsapp.js
+++ src/Components/SendToWhatsapp.js
@@ -2,25 +2,39 @@ import React, { Component } from "react";
 
 export default class SendToWhatsapp extends Component {
   state = {
-    wnumber: '994',
-  }
+    wnumber: "994",
+    wnumbers: new Array(),
+  };
 
   onChange = (e) => {
-    this.setState({wnumber: e.target.value});
-    window.localStorage.setItem('wnumber', e.target.value);
-  }
+    this.setState({ wnumber: e.target.value });
+    window.localStorage.setItem("wnumber", e.target.value);
+  };
+
   onCliked = (e) => {
     this.openInNewTab(`https://wa.me/${this.state.wnumber}`);
-  }
-  
+
+    let nums = this.state.wnumbers;
+    if (nums == null) nums = new Array();
+    nums.push(this.state.wnumber);
+    nums = nums.reverse();
+    let wnumberJson = JSON.stringify(nums);
+    window.localStorage.setItem("wnumbers", wnumberJson);
+    this.setState({ wnumbers: nums });
+  };
+
   openInNewTab = (url) => {
-    window.open(url, '_blank', 'noreferrer');
+    window.open(url, "_blank", "noreferrer");
   };
 
   componentDidMount() {
-    let wnumber = window.localStorage.getItem('wnumber');
-    wnumber = wnumber ?? '994';
+    let wnumber = window.localStorage.getItem("wnumber");
+    wnumber = wnumber ?? "994";
     this.setState({ wnumber, wnumber });
+
+    let wnumbers = window.localStorage.getItem("wnumbers");
+    wnumbers = JSON.parse(wnumbers);
+    this.setState({ wnumbers, wnumbers });
   }
 
   render() {
@@ -57,6 +71,15 @@ export default class SendToWhatsapp extends Component {
             </div>
           </div>
         </div>
+        <ul class="list-group">
+          {this.state.wnumbers?.map((elem) =>
+            elem.toString().length > 0 ? (
+              <li className="list-group-item">{elem}</li>
+            ) : (
+              ""
+            )
+          )}
+        </ul>
       </div>
     );
   }
