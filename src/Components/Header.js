4a7ddfc094d5c83c16edbbfb2feeebc41329c9d6
RustamZakAs <rostik055@gmail.com>
2023-01-18 12:57:01 +0400

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,6 +10,7 @@ export default class Header extends Component {
       <div className='header'>
         <div className='header-img-div'>
           <img className='header-img' src={this.props.formCode === 'C' ? imgC : this.props.formCode === 'W' ? imgW : imgI}></img>
+          <span className='header-text'>{this.props.formCode === 'C' ? "Comparer" : this.props.formCode === 'W' ? "Send to Whatsapp" : this.props.formCode === 'I' ? "Info" : ""}</span>
         </div>
       </div>
     )
